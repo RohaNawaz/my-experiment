699f767d56ee734663a4358095b4182bce4a4517
RohaNawaz <101508514+RohaNawaz@users.noreply.github.com>
2022-11-22 07:38:45 +0500

    building app

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,56 +1,98 @@
 import { PrismaClient } from "@prisma/client";
-const db = new PrismaClient();
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
 
 async function seed() {
-  const kody = await db.user.create({
+  const email = "rachel@remix.run";
+
+  // cleanup the existing database
+  await prisma.user.delete({ where: { email } }).catch(() => {
+    // no worries if it doesn't exist yet
+  });
+
+  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+
+  const user = await prisma.user.create({
     data: {
-      username: "kody",
-      // this is a hashed version of "twixrox"
-      passwordHash:
-        "$2b$10$K7L1OJ45/4Y2nIvhRVpCe.FSmhDdWoXehVzJptJ/op0lSsvqNu/1u",
+      email,
+      password: {
+        create: {
+          hash: hashedPassword,
+        },
+      },
     },
   });
-  await Promise.all(
-    getJokes().map((joke) => {
-      const data = { jokesterId: kody.id, ...joke };
-      return db.joke.create({ data });
-    })
-  );
-}
 
-seed();
+  await prisma.note.create({
+    data: {
+      title: "My first note",
+      body: "Hello, world!",
+      userId: user.id,
+    },
+  });
 
-function getJokes() {
-  // shout-out to https://icanhazdadjoke.com/
+  await prisma.note.create({
+    data: {
+      title: "My second note",
+      body: "Hello, world!",
+      userId: user.id,
+    },
+  });
 
-  return [
+  const posts = [
     {
-      name: "Road worker",
-      content: `I never wanted to believe that my Dad was stealing from his job as a road worker. But when I got home, all the signs were there.`,
+      slug: "my-first-post",
+      title: "My First Post",
+      markdown: `
+  # This is my first post
+  
+  Isn't it great?
+      `.trim(),
     },
     {
-      name: "Frisbee",
-      content: `I was wondering why the frisbee was getting bigger, then it hit me.`,
-    },
-    {
-      name: "Trees",
-      content: `Why do trees seem suspicious on sunny days? Dunno, they're just a bit shady.`,
-    },
-    {
-      name: "Skeletons",
-      content: `Why don't skeletons ride roller coasters? They don't have the stomach for it.`,
-    },
-    {
-      name: "Hippos",
-      content: `Why don't you find hippopotamuses hiding in trees? They're really good at it.`,
-    },
-    {
-      name: "Dinner",
-      content: `What did one plate say to the other plate? Dinner is on me!`,
-    },
-    {
-      name: "Elevator",
-      content: `My first time using an elevator was an uplifting experience. The second time let me down.`,
+      slug: "90s-mixtape",
+      title: "A Mixtape I Made Just For You",
+      markdown: `
+  # 90s Mixtape
+  
+  - I wish (Skee-Lo)
+  - This Is How We Do It (Montell Jordan)
+  - Everlong (Foo Fighters)
+  - Ms. Jackson (Outkast)
+  - Interstate Love Song (Stone Temple Pilots)
+  - Killing Me Softly With His Song (Fugees, Ms. Lauryn Hill)
+  - Just a Friend (Biz Markie)
+  - The Man Who Sold The World (Nirvana)
+  - Semi-Charmed Life (Third Eye Blind)
+  - ...Baby One More Time (Britney Spears)
+  - Better Man (Pearl Jam)
+  - It's All Coming Back to Me Now (Céline Dion)
+  - This Kiss (Faith Hill)
+  - Fly Away (Lenny Kravits)
+  - Scar Tissue (Red Hot Chili Peppers)
+  - Santa Monica (Everclear)
+  - C'mon N' Ride it (Quad City DJ's)
+      `.trim(),
     },
   ];
+  
+  for (const post of posts) {
+    await prisma.post.upsert({
+      where: { slug: post.slug },
+      update: post,
+      create: post,
+    });
+  }
+
+  console.log(`Database has been seeded. 🌱`);
 }
+
+seed()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
