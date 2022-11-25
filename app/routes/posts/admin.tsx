720571176e11be384617fe7332a5e2b892da9310
RohaNawaz <101508514+RohaNawaz@users.noreply.github.com>
2022-11-25 11:32:25 +0500

    Deploying app

--- app/routes/posts/admin.tsx
+++ app/routes/posts/admin.tsx
@@ -1,6 +1,6 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Link, Outlet, useLoaderData } from "@remix-run/react";
+import { Link, Outlet, Path, useLoaderData } from "@remix-run/react";
 
 import { getPosts } from "~/models/post.server";
 
